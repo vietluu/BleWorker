0c47460b0596284b1ba55c9e526bae276f3b313d
luu viet <vietluu24012001@gmail.com>
2023-12-18 14:07:25 +0700

    fix runtime

--- js/index.js
+++ js/index.js
@@ -16,25 +16,27 @@ startBtn.addEventListener("click", start);
 endBtn.addEventListener("click", end);
 shutdownBtn.addEventListener("click", shutdown);
 powerBtn.addEventListener("click", powerQuery);
+let i = 0;
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("./js/service-worker.js")
+    .register("/service-worker.js",{scope: '/'})
     .then(function (registration) {
       console.log("Registration successful, scope is:", registration.scope);
+   
+      navigator.serviceWorker.addEventListener('message', function(event) {
+        if (event.data && event.data.action === 'next-action-ready') {
+          // Thực hiện lệnh ở đây khi nhận được thông điệp từ Service Worker
+          console.log('Running code in index.js every 10 minutes');
+          value.innerHTML = `Running code in index.js every 10 minutes ${++i}`;
+        }
+      });
+
     })
     .catch(function (error) {
       console.log("Service worker registration failed, error:", error);
     });
-    navigator.serviceWorker.ready.then((registration) => {
-      registration.sync.register('measurement-sync');
-    });
+   
     
-    navigator.serviceWorker.addEventListener('message', (event) => {
-      if (event.data && event.data.action === 'start-measurement') {
-        // Gọi hàm start từ index khi nhận được tin nhắn từ Service Worker
-        start();
-      }
-    });
 }
 async function connection() {
   const data = new Uint8Array([0xcc, 0x80, 0x02, 0x03, 0x01, 0x01, 0x00, 0x01]);
@@ -74,33 +76,44 @@ async function powerQuery() {
   if(byteArray.length == 20){
     value.innerHTML = `result - Sys :${byteArray[14]} , Dia: ${byteArray[16]} , pulse: ${byteArray[18]}`;
     status.innerHTML = "Đã đo xong";
+    if ('serviceWorker' in navigator && navigator.serviceWorker.controller) {
+      navigator.serviceWorker.controller.postMessage({ action: 'schedule-next-action' });
+    }
   }
   if(byteArray.length == 8 && byteArray[6] != 0){
     status.innerHTML = "";
     value.innerHTML = `error code: ${byteArray[6]}`;
+    if ('serviceWorker' in navigator && navigator.serviceWorker.controller) {
+      navigator.serviceWorker.controller.postMessage({ action: 'schedule-next-action' });
+    }
   }
 }
 let intervalId; // Biến lưu trữ ID của interval
-
 export async function start() {
   status.innerHTML = "Đang tiền hành đo...";
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(function(registration) {
+      // Gửi thông điệp tới service worker
+      registration.active.postMessage({action: 'schedule-next-action'});
+    });
+  }
   try {
     const data = new Uint8Array([
       0xcc, 0x80, 0x02, 0x03, 0x01, 0x02, 0x00, 0x02,
     ]);
     await characteristic.writeValue(data);
 
-    if (intervalId) {
-      clearInterval(intervalId);
-    }
+    // if (intervalId) {
+    //   clearInterval(intervalId);
+    // }
 
-    intervalId = setInterval(async () => {
-      await start(); // Gọi lại start sau mỗi 30 phút
-    }, 30 * 60 * 1000); // 30 phút * 60 giây/phút * 1000 milliseconds/giây
+    // intervalId = setInterval(async () => {
+    //   await start(); // Gọi lại start sau mỗi 30 phút
+    // }, 30 * 60 * 1000); // 30 phút * 60 giây/phút * 1000 milliseconds/giây
 
   } catch (error) {
     alert("Lỗi kết nối: " + error);
-    window.location.reload();
+  //  window.location.reload();
     console.log(error);
   }
 }
---
+++ service-worker.js
@@ -0,0 +1,41 @@
+// service-worker.js
+
+// Khi Service Worker được khởi tạo
+self.addEventListener('install', function(event) {
+  event.waitUntil(self.skipWaiting()); // Activate worker immediately
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+  clients.claim();
+  event.waitUntil(self.clients.claim()); // Become available to all pages
+});
+
+ // Khi nhận được tin nhắn từ trang web
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.action === 'schedule-next-action') {
+    console.log('run')
+    scheduleNextAction();
+  }
+});
+// Hàm thực hiện công việc sau mỗi 10 phút
+function scheduleNextAction() {
+  setTimeout(() => {
+    // Gọi hàm khác bạn muốn kích hoạt sau mỗi 10 phút ở đây
+    // Ví dụ:
+    // otherFunction();
+    console.log('Other function activated after 10 minutes');
+
+    // Gửi tin nhắn tới trang web để thông báo rằng đã qua 10 phút và có thể kích hoạt hàm khác tiếp theo
+    self.clients.matchAll({includeUncontrolled: true,
+      type: 'window',}).then((clients) => {
+        console.log(clients)
+      clients.forEach((client) => {
+        client.postMessage({ action: 'next-action-ready' });
+      });
+    });
+
+    // Lặp lại việc gọi hàm này sau mỗi 10 phút
+    scheduleNextAction();
+  },  10 * 60 * 1000); // 10 phút * 60 giây/phút * 1000 milliseconds/giây
+}
