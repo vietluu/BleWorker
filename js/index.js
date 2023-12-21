975b81c8dd375a7a18fc0c1020271c38025a0840
luu viet <vietluu24012001@gmail.com>
2023-12-21 17:23:53 +0700

    com

--- js/index.js
+++ js/index.js
@@ -17,27 +17,52 @@ endBtn.addEventListener("click", end);
 shutdownBtn.addEventListener("click", shutdown);
 powerBtn.addEventListener("click", powerQuery);
 let i = 0;
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("./service-worker.js",{scope: '/BleWorker/'})
-    .then(function (registration) {
-      console.log("Registration successful, scope is:", registration.scope);
-   
-      navigator.serviceWorker.addEventListener('message', function(event) {
-        if (event.data && event.data.action === 'next-action-ready') {
-          // Thực hiện lệnh ở đây khi nhận được thông điệp từ Service Worker
-          console.log('Running code in index.js every 10 minutes');
-          value.innerHTML = `Running code in index.js every 10 minutes ${++i}`;
-        }
-      });
+window.addEventListener('load',async  function() {
+  await window.Notification.requestPermission();
+  await navigator.permissions.query({
+    name: 'periodic-background-sync',
+  });
+   //Notification.requestPermission()
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/service-worker.js", { scope: "./" })
+      .then(function (registration) {
+        
+        console.log("Registration successful, scope is:", registration.scope);
+     
+        navigator.serviceWorker.addEventListener('message', function(event) {
+          if (event.data && event.data.action === 'next-action-ready') {
+            // Thực hiện lệnh ở đây khi nhận được thông điệp từ Service Worker
+            status.innerHTML = 'Running code in index.js'
 
-    })
-    .catch(function (error) {
-      console.log("Service worker registration failed, error:", error);
-    });
-   
-    
+            console.log('Running code in index.js every 10 minutes');
+            navigator.serviceWorker.ready.then(function(registration) {
+              // Gửi thông điệp tới service worker
+              registration.active.postMessage({action: 'doing-action'});
+            });
+            setTimeout(() => {
+              start();
+            }, 3 * 60 * 1000);
+          }
+        });
+  
+      })
+      .catch(function (error) {
+        console.log("Service worker registration failed, error:", error);
+      });
+      navigator.serviceWorker.ready.then(registration => {
+        registration.sync.register('background-sync-task')
+          .then(() => {
+            console.log('Background sync registered');
+          })
+          .catch(error => {
+            console.error('Background sync registration failed:', error);
+          });
+      });
+      
+  }
 }
+)
 async function connection() {
   const data = new Uint8Array([0xcc, 0x80, 0x02, 0x03, 0x01, 0x01, 0x00, 0x01]);
   await characteristic.writeValue(data);
@@ -68,51 +93,51 @@ async function powerQuery() {
 
   // Chuyển dữ liệu hexa sang mảng byte
   const byteArray = new Uint8Array(receivedData.buffer);
-  list.innerHTML = `${formattedDateTime}: Received data: ` + byteArray;
+ // list.innerHTML = `${formattedDateTime}: Received data: ` + byteArray;
   if(byteArray.length == 13){
     value.innerHTML = `processing: ${byteArray[10]}`;
   }
+  let div  = document.createElement("p");
+  div.innerHTML =  `<p>${formattedDateTime}: Received data:  + ${byteArray} </p>`;
+  list.appendChild(div);
   console.log(`${formattedDateTime}: Received data:`, byteArray);
   if(byteArray.length == 20){
     value.innerHTML = `result - Sys :${byteArray[14]} , Dia: ${byteArray[16]} , pulse: ${byteArray[18]}`;
     status.innerHTML = "Đã đo xong";
-    if ('serviceWorker' in navigator && navigator.serviceWorker.controller) {
-      navigator.serviceWorker.controller.postMessage({ action: 'schedule-next-action' });
+    let div  = document.createElement("p");
+    div.innerHTML =  `<p>${formattedDateTime}: Received result:  + ${byteArray} </p>`;
+    list.appendChild(div);
+    if ('serviceWorker' in navigator) {
+      
+      navigator.serviceWorker.ready.then(function(registration) {
+        // Gửi thông điệp tới service worker
+        registration.active.postMessage({action: 'schedule-next-action'});
+      });
     }
   }
+
   if(byteArray.length == 8 && byteArray[6] != 0){
     status.innerHTML = "";
     value.innerHTML = `error code: ${byteArray[6]}`;
-    if ('serviceWorker' in navigator && navigator.serviceWorker.controller) {
-      navigator.serviceWorker.controller.postMessage({ action: 'schedule-next-action' });
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready.then(async function(registration) {
+        // Gửi thông điệp tới service worker
+        await registration.active.postMessage({action: 'schedule-next-action'});
+      });
     }
   }
 }
 let intervalId; // Biến lưu trữ ID của interval
 export async function start() {
   status.innerHTML = "Đang tiền hành đo...";
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(function(registration) {
-      // Gửi thông điệp tới service worker
-      registration.active.postMessage({action: 'schedule-next-action'});
-    });
-  }
+ 
   try {
     const data = new Uint8Array([
       0xcc, 0x80, 0x02, 0x03, 0x01, 0x02, 0x00, 0x02,
     ]);
     await characteristic.writeValue(data);
-
-    // if (intervalId) {
-    //   clearInterval(intervalId);
-    // }
-
-    // intervalId = setInterval(async () => {
-    //   await start(); // Gọi lại start sau mỗi 30 phút
-    // }, 30 * 60 * 1000); // 30 phút * 60 giây/phút * 1000 milliseconds/giây
-
   } catch (error) {
-    alert("Lỗi kết nối: " + error);
+   // alert("Lỗi kết nối: " + error);
   //  window.location.reload();
     console.log(error);
   }
--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,131 @@
 // service-worker.js
 
 // Khi Service Worker được khởi tạo
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
+  self.skipWaiting(); // Kích hoạt Service Worker ngay lập tức
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('Activated', event);
-// Become available to all pages
+self.addEventListener("activate", function (event) {
+  console.log("Activated", event);
+  
 });
-
- // Khi nhận được tin nhắn từ trang web
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.action === 'schedule-next-action') {
-    console.log('run')
-    scheduleNextAction();
+self.addEventListener("message", (event) => {
+  console.log("Received message from main thread", event.data);
+  if (event.data && event.data.action === "schedule-next-action") {
+    console.log("run");
+    event.waitUntil(scheduleNextAction());
+  }
+  if (event.data && event.data.action === "doing-action") {
+    console.log("run");
+    event.waitUntil(
+      self.registration.showNotification("Globi", {
+        body: "Uứng dụng đang đo...",
+        data: {
+          url: "https://2gt04bc6-5502.asse.devtunnels.ms/",
+        },
+      })
+    );
   }
 });
-// Hàm thực hiện công việc sau mỗi 10 phút
+
+// async function start() {
+//  // status.innerHTML = "Đang tiền hành đo...";
+ 
+//   try {
+//     const data = new Uint8Array([
+//       0xcc, 0x80, 0x02, 0x03, 0x01, 0x02, 0x00, 0x02,
+//     ]);
+//     await characteristic.writeValue(data);
+//   } catch (error) {
+//    // alert("Lỗi kết nối: " + error);
+//   //  window.location.reload();
+//     console.log(error);
+//   }
+// }
+
+// Đăng ký sự kiện sync
+self.addEventListener('sync', event => {
+  if (event.tag === 'background-sync-task') {
+    event.waitUntil(doBackgroundSync());
+  }
+});
+
+function doBackgroundSync() {
+  // Thực hiện công việc cần thiết ở đây
+  console.log('Doing background sync...');
+  // Ví dụ: gửi dữ liệu lên server
+  fetch('/')
+    .then(response => {
+      // Xử lý kết quả nếu cần
+      console.log('Sync successful');
+    })
+    .catch(error => {
+      console.error('Sync failed:', error);
+    });
+}
+
+// Đăng ký periodic sync (định kỳ)
+self.addEventListener('activate', event => {
+  event.waitUntil(registerPeriodicSync());
+});
+
+async function registerPeriodicSync() {
+  if ('periodicSync' in self.registration) {
+    try {
+      await self.registration.periodicSync.register('background-sync-task', {
+        minInterval: 3 * 60 * 1000, // Định kỳ 24 giờ
+      });
+      console.log('Periodic sync registered');
+    } catch (error) {
+      console.error('Periodic sync registration failed:', error);
+    }
+  } else {
+    console.error('Periodic background sync not supported');
+  }
+}
+
+
 function scheduleNextAction() {
-  setTimeout(() => {
-    // Gọi hàm khác bạn muốn kích hoạt sau mỗi 10 phút ở đây
-    // Ví dụ:
-    // otherFunction();
-    console.log('Other function activated after 10 minutes');
+    console.log("Other function activated after 10 minutes");
 
     // Gửi tin nhắn tới trang web để thông báo rằng đã qua 10 phút và có thể kích hoạt hàm khác tiếp theo
-    self.clients.matchAll({includeUncontrolled: true,
-      type: 'window',}).then((clients) => {
-        console.log(clients)
-      clients.forEach((client) => {
-        client.postMessage({ action: 'next-action-ready' });
+    self.clients
+      .matchAll({
+        includeUncontrolled: true,
+        type: "all",
+      })
+      .then((clients) => {
+        console.log(clients);
+        clients.forEach((client) => {
+          self.registration.showNotification("Globi", {
+            body: "Uứng dụng bắt đầu đo sau 3p...",
+            data: {
+              url: "https://2gt04bc6-5502.asse.devtunnels.ms/",
+            },
+          });
+          client.postMessage({ action: "next-action-ready" });
+        });
       });
-    });
-
-    // Lặp lại việc gọi hàm này sau mỗi 10 phút
-    scheduleNextAction();
-  },  60 * 1000); // 10 phút * 60 giây/phút * 1000 milliseconds/giây
 }
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close(); // Đóng thông báo
+  const urlToOpen = event.notification.data.url; // Lấy URL từ dữ liệu thông báo
+
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: "window",
+        includeUncontrolled: true,
+      })
+      .then((clients) => {
+        // Kiểm tra xem có cửa sổ nào đang mở không
+        for (let client of clients) {
+          if (client.url === urlToOpen && "focus" in client) {
+            return client.focus(); // Focus vào cửa sổ đang mở có URL tương ứng
+          }
+        }
+        // Nếu không có cửa sổ nào mở với URL tương ứng, mở một cửa sổ mới
+        return clients.openWindow(urlToOpen);
+      })
+  );
+});
